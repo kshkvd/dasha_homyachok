746abbd8a10230cdd27f44b7d0c4de79bae34646
kshkvd <daryakosh-ipt28@lll.kpi.ua>
2026-01-08 20:02:55 +0200

    hw1

---
+++ hw1.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//1
+const hamster = {};
+
+//2
+hamster.name = 'Dasha';
+hamster.type = 'Hamster';
+hamster.age = 1;
+hamster.color = 'grey';
+
+//3
+hamster.age = 2;
+hamster.color = 'golden';
+
+//4
+const hamsterActivities = [];
+hamsterActivities.push('Eat');
+hamsterActivities.push('Run in wheel');
+hamsterActivities.push('Sleep');
+
+//5
+console.log('Hamster object:', hamster);
+console.log('Hamster activities:', hamsterActivities);
